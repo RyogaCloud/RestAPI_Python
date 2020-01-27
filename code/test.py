8fd68d798c3cbb378098ff3e74a96f87fc27d000
RyogaTakao <170441089@ccalumni.meijo-u.ac.jp>
2020-01-27 14:45:24 +0900

    更新

--- code/test.py
+++ code/test.py
@@ -1,1 +1,11 @@
-import
+from flask import Flask, request, render_template, redirect
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return 'Hello'
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=3000, threaded=True)
